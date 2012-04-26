3fec5274874720b31c8ba1d214e05e5156066a3d
Inshan <Inshan@10.164.147.232>
2012-04-26 19:48:13 -0400

    Main servlet connection added

--- src/MainServlet.java
+++ src/MainServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DataAccess.DataAccessLayer;
+
 /**
  * Servlet implementation class MainServlet
  */
@@ -29,6 +31,8 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		DataAccessLayer DAO = DataAccessLayer.getInstance();
+		DAO.closeDbConnection();
 		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/test.jsp");
 		dispatcher.forward(request, response);
 	}
