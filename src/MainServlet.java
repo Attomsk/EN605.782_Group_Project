f44cabca7240816aae5a8414e36eafbeba796b9d
Benjamin Morlok <bmorlok@gmail.com>
2012-04-26 19:23:01 -0400

    Fixed the test servlet

--- src/MainServlet.java
+++ src/MainServlet.java
@@ -29,7 +29,7 @@ public class MainServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/welcome.jsp");
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/test.jsp");
 		dispatcher.forward(request, response);
 	}
 
